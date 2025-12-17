46f319fb67a2d255670220ddbe84ba40c7f9e7a7
bala-amypo <bala@amypo.in>
2025-12-17 04:50:27 +0000

    User commit

---
+++ src/main/java/com/example/demo/Service/DepartmentService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+public interface DepartmentService {
+    Department create(Department department);
+    Department get(Long id);
+    void delete(Long id);
+    List<Department> getAll();
+}
---
+++ src/main/java/com/example/demo/Service/ShiftTemplateService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+public interface ShiftTemplateService {
+    ShiftTemplate create(ShiftTemplate template);
+    List<ShiftTemplate> getByDepartment(Long departmentId);
+}
