f91665f4fb78cebe800efb5e5df4c0ac2bfcec0e
bala-amypo <bala@amypo.in>
2025-12-17 10:35:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,32 +1,32 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
-
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @Tag(name = "Authentication Endpoints")
 public class AuthController {
+
     @Autowired
-    UserService userserv;
+    private UserService userService;
+
     @PostMapping("/register")
-    public User register(User user) {
-        return userserv.register(user);
+    public User register(@RequestBody User user) {
+        return userService.register(user);
     }
+
     @PostMapping("/login")
-    public String login(String email, String password) {
-        User user = userserv.findByEmail(email);
+    public String login(@RequestParam String email,@RequestParam String password) {
+
+        User user = userService.findByEmail(email);
+
         if (user != null && user.getPassword().equals(password)) {
             return "Login successful";
-        } else {
-            return "Invalid email or password";
         }
+        return "Invalid email or password";
     }
 }
