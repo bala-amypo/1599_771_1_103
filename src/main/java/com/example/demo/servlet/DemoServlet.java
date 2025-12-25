f10fe231fb95bc4e8b879213ef7274ba53554b0a
bala-amypo <bala@amypo.in>
2025-12-25 06:54:50 +0000

    User commit

--- src/main/java/com/example/demo/servlet/DemoServlet.java
+++ src/main/java/com/example/demo/servlet/DemoServlet.java
@@ -2,7 +2,6 @@ package com.example.demo.servlet;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,10 +12,7 @@ public class DemoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response)
-            throws ServletException, IOException {
-
-        response.setStatus(HttpServletResponse.SC_OK);
+                         HttpServletResponse response) throws IOException {
         response.getWriter().write("Servlet Running");
     }
 }
