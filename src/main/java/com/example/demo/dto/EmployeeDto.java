4b7d9dd8ea91b74e7b157aaa286a9a08242496fb
bala-amypo <bala@amypo.in>
2025-12-26 09:23:00 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeDto.java
+++ src/main/java/com/example/demo/dto/EmployeeDto.java
@@ -2,46 +2,57 @@ package com.example.demo.dto;
 
 public class EmployeeDto {
 
+    private Long id;
     private String fullName;
     private String email;
     private String role;
     private String skills;
     private Integer maxWeeklyHours;
 
-    public String getFullName() {
-        return fullName;
+    public EmployeeDto() {}
+
+    public Long getId() {
+        return id;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public String getFullName() {
+        return fullName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getRole() {
         return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
     }
 
     public String getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
-
     public Integer getMaxWeeklyHours() {
         return maxWeeklyHours;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
     }
 
     public void setMaxWeeklyHours(Integer maxWeeklyHours) {
